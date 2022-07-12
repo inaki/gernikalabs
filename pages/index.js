8ca96eff1d6bf8703c6e19513c90f9bda321410e
Iñaki Aranzadi <inaki.aranzadi@withjoy.com>
2022-07-12 16:46:17 -0500

    Add files to repo

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,30 @@
 import Head from 'next/head'
+import Link from 'next/link';
+import Profile from './posts/components/Profile';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Yo Yo Yo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
+        <Profile />
+
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learning <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Let's see what is possible
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <hr/>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <Link href="/posts/first-post">First Post</Link>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
@@ -54,8 +33,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <span style={{color: 'rebeccapurple'}}>&nbsp; Gernika Labs &nbsp;</span> with 	&#x2764;.
         </a>
       </footer>
 
---
+++ pages/posts/first-post.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+    return  <div>
+        <Link href={'/'}>home</Link>
+        <h1>First Post</h1>
+    </div>;
+  }
